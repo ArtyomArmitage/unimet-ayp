6735130191a3f802e1550bc7e42d76a53f2da75a
ArtyomArmitage <mujicam@correo.unimet.edu.ve>
2025-01-08 08:12:27 -0400

    Update e1.py

--- 2-variables/e1.py
+++ 2-variables/e1.py
@@ -3,3 +3,9 @@ import typing
 volumen_reservorio: float = 4.445e8
 lluvia: float = 5e6
 
+lluvia=lluvia*0.9
+volumen_reservorio+=lluvia
+volumen_reservorio=volumen_reservorio*(1+0.05)
+volumen_reservorio=volumen_reservorio*0.98
+volumen_reservorio-=2.5e5
+print(volumen_reservorio)
